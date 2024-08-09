e61b946981b6e70c2ae6350622198550711cacf5
sgoudham <sgoudham@gmail.com>
2024-08-09 17:36:27 +0100

    build: remove teamcity dependencies
    I don't believe we need these

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
-        maven("https://packages.jetbrains.team/maven/p/teamcity-rest-client/teamcity-rest-client")
-        maven("https://download.jetbrains.com/teamcity-repository")
         maven("https://packages.jetbrains.team/maven/p/fleet/fleet-sdk")
     }
 }
