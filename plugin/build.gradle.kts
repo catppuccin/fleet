94b19c097b83e6da0e498fe77f3b02493e3c5f44
sgoudham <sgoudham@gmail.com>
2024-08-09 18:11:51 +0100

    build: remove publishing block

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 version = "0.1.0"
 
 fleetPlugin {
-
     id = "com.github.catppuccin.fleet"
 
     metadata {
@@ -22,14 +21,4 @@ fleetPlugin {
     fleetRuntime {
         version = libs.versions.fleet.runtime
     }
-
-    // FIXME: For some reason, uncommenting these lines makes compilation fail due to unresolved references
-    // Unsure if it is due to them only being available when publishing or what is up?
-    //
-    // The disassembly of `publishing` specifies that only `channel`, `marketplaceUrl`, `token` and `vendorId`
-    // can be specified
-//    publishing {
-//        licenseUrl = "https://github.com/catppuccin/fleet/blob/main/LICENSE"
-//        sourceCodeUrl = "https://github.com/catppuccin/fleet"
-//    }
 }
